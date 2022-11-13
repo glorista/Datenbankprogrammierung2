cafbc1511654a4fa2478299705d9c30c8c3d7dc8
mario <mario@mosa.at>
2022-11-13 19:24:27 +0100

    changes

--- src/main/java/at/campus02/dbp2/repository/Application.java
+++ src/main/java/at/campus02/dbp2/repository/Application.java
@@ -9,34 +9,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        log("application started");
 
-//        CustomerRepository repository = new InMemoryRepository();
-//        CustomerRepository repository = new JdbcRepository("jdbc:derby:database;create=true");
-        CustomerRepository repository = new JpaRepository();
-
-        Customer customer = new Customer();
-        customer.setEmail("customer1@customers.com");
-        customer.setLastname("Customer");
-        customer.setFirstname("Carlo");
-
-        // 1) Create
-        repository.create(customer);
-        log("Customer created: " + customer);
-
-        // 2) Read
-        Customer fromRepository = repository.read(customer.getEmail());
-        log("Customer read: " + fromRepository);
-
-        // 3) Update
-        fromRepository.setFirstname("Conrad");
-        repository.update(fromRepository);
-        Customer updated = repository.read(fromRepository.getEmail());
-        log("Customer updated: " + updated);
-
-        // 4) Delete
-        repository.delete(updated);
-        Customer deleted = repository.read(updated.getEmail());
-        log("Customer deleted: " + deleted);
     }
 }
